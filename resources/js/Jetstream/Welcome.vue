4f055e4e16918ee120821b1f1ecc18ef59f5bcc3
Serhii Husach <sergeygusach@gmail.com>
2020-09-22 11:39:12 +0300

    Initial project version

---
+++ resources/js/Jetstream/Welcome.vue
@@ -0,0 +1,188 @@
+<template>
+  <div>
+    <div class="bg-white border-b border-gray-200">
+      <div>
+        <div class="container mx-auto">
+          <div>
+
+
+            <div class="-mx-4 sm:-mx-8 px-4 sm:px-8 overflow-x-auto">
+              <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                <table class="min-w-full leading-normal">
+                  <thead>
+                    <tr>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >User</th>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >Time</th>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >Mentoring</th>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >Responsibility</th>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >Codestyle</th>
+                      <th
+                        class="px-5 py-3 border-b-2 border-gray-200 bg-green-100  text-center text-xs font-semibold text-gray-600 uppercase tracking-wider"
+                      >Result</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                              alt
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">Vera Carpenter</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">168</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">21</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">15</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">10</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">214</p>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                              alt
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">Blake Bowman</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">160</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">18</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">12</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">12</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">202</p>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1540845511934-7721dd7adec3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                              alt
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap">Dana Moore</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">152</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">12</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">15</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">12</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">192</p>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <div class="flex items-center">
+                          <div class="flex-shrink-0 w-10 h-10">
+                            <img
+                              class="w-full h-full rounded-full"
+                              src="https://images.unsplash.com/photo-1522609925277-66fea332c575?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&h=160&w=160&q=80"
+                              alt
+                            />
+                          </div>
+                          <div class="ml-3">
+                            <p class="text-gray-900 whitespace-no-wrap text-center">Alonzo Cox</p>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">160</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">12</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">8</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">6</p>
+                      </td>
+                      <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                        <p class="text-gray-900 whitespace-no-wrap text-center">186</p>
+                      </td>
+
+                    </tr>
+
+                  </tbody>
+                </table>
+                <div
+                    class="px-5 py-5 bg-white flex flex-col xs:flex-row items-center xs:justify-between">
+                    some space
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import JetApplicationLogo from "./../Jetstream/ApplicationLogo";
+
+export default {
+  components: {
+    JetApplicationLogo,
+  },
+};
+</script>
